7f45a44423de991f644db2f418f5c21278df0ffe
Sebastian Buczyński <nnplaya@gmail.com>
2022-11-27 22:57:47 +0100

    Implement negotiation factory

---
+++ used_stuff_market/negotiations/negotiations/factory.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+
+from negotiations.negotiation import Negotiation
+
+
+class NotABuyerOrSeller(Exception):
+    pass
+
+
+class InvalidPrice(ValueError):
+    pass
+
+
+def build_negotiation(
+    user_id: int,
+    item_id: int,
+    seller_id: int,
+    buyer_id: int,
+    price: Decimal,
+    currency: str,
+) -> Negotiation:
+    if user_id not in (seller_id, buyer_id):
+        raise NotABuyerOrSeller
+
+    if price <= 0:
+        raise InvalidPrice
+
+    return Negotiation(
+        item_id=item_id,
+        buyer_id=buyer_id,
+        seller_id=seller_id,
+        price=price,
+        currency=currency,
+        waits_for_decision_of=seller_id,
+    )
