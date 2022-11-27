b000bfaddb30b2593992b5e43cd8f2462ac7e955
Sebastian Buczyński <nnplaya@gmail.com>
2022-11-27 22:32:42 +0100

    Implement Negotiation aggregate and use it in views

--- used_stuff_market/negotiations/negotiations/negotiation.py
+++ used_stuff_market/negotiations/negotiations/negotiation.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from pydantic import BaseModel
 
 
@@ -5,3 +7,25 @@ class Negotiation(BaseModel):
     item_id: int
     seller_id: int
     buyer_id: int
+    price: Decimal
+    currency: str
+    broken_off: bool = False
+    accepted: bool = False
+
+    class NegotiationConcluded(Exception):
+        pass
+
+    class OnlySellerCanAccept(Exception):
+        pass
+
+    def break_off(self) -> None:
+        if self.broken_off or self.accepted:
+            raise self.NegotiationConcluded
+        self.broken_off = True
+
+    def accept(self, user_id: int) -> None:
+        if self.broken_off or self.accepted:
+            raise self.NegotiationConcluded
+        if user_id != self.seller_id:
+            raise self.OnlySellerCanAccept
+        self.accepted = True
