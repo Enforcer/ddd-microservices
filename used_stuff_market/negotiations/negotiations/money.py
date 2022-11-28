77f6be022bb85fac6118d4b78171d7d8abdb4816
Sebastian Buczyński <nnplaya@gmail.com>
2022-11-28 21:54:55 +0100

    implement strategy and tests for it

--- used_stuff_market/negotiations/negotiations/money.py
+++ used_stuff_market/negotiations/negotiations/money.py
@@ -1,3 +1,4 @@
+import functools
 from decimal import Decimal
 from enum import StrEnum
 
@@ -8,6 +9,7 @@ class Currency(StrEnum):
     USD = "USD"
 
 
+@functools.total_ordering
 class Money(BaseModel):
     amount: Decimal
     currency: Currency
@@ -17,3 +19,17 @@ class Money(BaseModel):
         if raw_price <= 0:
             raise ValueError("Price must be positive")
         return raw_price
+
+    def __sub__(self, other: "Money") -> "Money":
+        if not isinstance(other, Money):
+            raise TypeError("Can only subtract Money from Money")
+        if self.currency != other.currency:
+            raise TypeError("Can only subtract same currency Money")
+        return Money(amount=self.amount - other.amount, currency=self.currency)
+
+    def __lt__(self, other: "Money") -> bool:
+        if not isinstance(other, Money):
+            raise TypeError("Can only compare Money with Money")
+        if self.currency != other.currency:
+            raise TypeError("Can only compare Money with same currency")
+        return self.amount < other.amount
