4bdcc55cba71b1e03d36336efe75b35095eb5afe
Sebastian Buczyński <nnplaya@gmail.com>
2022-12-05 21:24:49 +0100

    Protect against duplicates

--- used_stuff_market/catalog/catalog/api.py
+++ used_stuff_market/catalog/catalog/api.py
@@ -8,6 +8,6 @@ app = FastAPI()
 def search(term: str):
     collection = getattr(db.get(), "catalog_items")
     result = collection.find(
-        {"$text": {"$search": term}}, projection={"_id": False}
+        {"$text": {"$search": term}}, projection={"_id": False, "version": False}
     ).sort("score", {"$meta": "textScore"})
     return list(result)
