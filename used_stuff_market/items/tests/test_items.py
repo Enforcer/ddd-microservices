f8667eb0030b14d4350a3ee4419dfc1fe58e0248
Sebastian Buczyński <nnplaya@gmail.com>
2022-12-05 20:54:19 +0100

    Name queues and implement test

--- used_stuff_market/items/tests/test_items.py
+++ used_stuff_market/items/tests/test_items.py
@@ -1,8 +1,10 @@
 from typing import Iterator
 
+import mqlib.testing
 import pytest
 from fastapi.testclient import TestClient
 from items.api import app
+from items.queues import item_added
 
 
 @pytest.fixture()
@@ -41,6 +43,31 @@ def test_added_item_is_available(client: TestClient) -> None:
     ]
 
 
+def test_message_about_new_item_is_sent(client: TestClient) -> None:
+    mqlib.testing.purge(queue=item_added)
+    post_response = client.post(
+        "/items",
+        json={
+            "title": "Lorem Ipsum Dolor Sit Amet",
+            "description": "Consectetur adipiscing elit.",
+            "price": {
+                "amount": 6.99,
+                "currency": "USD",
+            },
+        },
+        headers={"user-id": "3"},
+    )
+    assert post_response.status_code == 204
+
+    message = mqlib.testing.next_message(queue=item_added)
+    assert message == {
+        "item_id": 2,
+        "title": "Lorem Ipsum Dolor Sit Amet",
+        "description": "Consectetur adipiscing elit.",
+        "price": {"amount": "6.99", "currency": "6.99"},
+    }
+
+
 def test_update_of_item_is_applied(client: TestClient) -> None:
     post_response = client.post(
         "/items",
@@ -57,7 +84,7 @@ def test_update_of_item_is_applied(client: TestClient) -> None:
     assert post_response.status_code == 204
 
     post_response = client.put(
-        "/items/2",
+        "/items/3",
         json={
             "title": "JOKE THESE WERE JEANS",
             "description": "Plz buy",
@@ -74,7 +101,7 @@ def test_update_of_item_is_applied(client: TestClient) -> None:
     assert get_response.status_code == 200
     assert get_response.json() == [
         {
-            "id": 2,
+            "id": 3,
             "title": "JOKE THESE WERE JEANS",
             "description": "Plz buy",
             "price": {
