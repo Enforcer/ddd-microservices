9a1b48b45bf31c582c0babe281f52d014fa0779e
Sebastian Buczyński <nnplaya@gmail.com>
2022-12-05 22:08:29 +0100

    Deduplicate likes by introducing idempotent messages

--- used_stuff_market/catalog/catalog/consumer.py
+++ used_stuff_market/catalog/catalog/consumer.py
@@ -8,7 +8,7 @@ from catalog.queues import item_cdc, item_liked, item_unliked, setup_queues
 def on_item_change(body: dict, message: mqlib.Message) -> None:
     logging.info("Item CDC: %r", body)
     item_id = body["item_id"]
-    data = dao.get(item_id) or {"likes": 0}
+    data = dao.get(item_id) or {"likes": []}
     if data.get("version", 0) != 0 and data["version"] >= body["version"]:
         logging.warning("Duplicate detected!")
         return
@@ -20,17 +20,15 @@ def on_item_change(body: dict, message: mqlib.Message) -> None:
 def on_item_liked(body: dict, message: mqlib.Message) -> None:
     logging.info("Item liked: %r", body)
     item_id = body["item_id"]
-    data = dao.get(item_id) or {"likes": 0}
-    data["likes"] += 1
-    dao.upsert(item_id=body["item_id"], data=data)
+    liker_id = body["liker_id"]
+    dao.add_like(item_id, liker_id=liker_id)
 
 
 def on_item_unliked(body: dict, message: mqlib.Message) -> None:
     logging.info("Item unliked: %r", body)
     item_id = body["item_id"]
-    data = dao.get(item_id) or {"likes": 0}
-    data["likes"] -= 1
-    dao.upsert(item_id=body["item_id"], data=data)
+    liker_id = body["liker_id"]
+    dao.remove_like(item_id, liker_id=liker_id)
 
 
 if __name__ == "__main__":
--- used_stuff_market/catalog/catalog/dao.py
+++ used_stuff_market/catalog/catalog/dao.py
@@ -8,6 +8,24 @@ def upsert(item_id: int, data: dict) -> None:
     )
 
 
+def add_like(item_id: int, liker_id: int) -> None:
+    collection = getattr(db.get(), "catalog_items")
+    collection.update_one(
+        {"item_id": item_id},
+        {"$addToSet": {"likes": liker_id}},
+        upsert=True,
+    )
+
+
+def remove_like(item_id: int, liker_id: int) -> None:
+    collection = getattr(db.get(), "catalog_items")
+    collection.update_one(
+        {"item_id": item_id},
+        {"$pull": {"likes": liker_id}},
+        upsert=True,
+    )
+
+
 def get(item_id: int) -> dict | None:
     collection = getattr(db.get(), "catalog_items")
     return collection.find_one({"item_id": item_id}, projection={"_id": False})
