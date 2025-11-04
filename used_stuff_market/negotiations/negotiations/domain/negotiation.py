c43fcbe9e7dd4f42b2f7bf3475ee01b10da0146d
Sebastian Buczyński <nnplaya@gmail.com>
2025-11-04 20:55:39 +0100

    Add starting point for Negotiation aggregate implementation

--- used_stuff_market/negotiations/negotiations/domain/negotiation.py
+++ used_stuff_market/negotiations/negotiations/domain/negotiation.py
@@ -1,4 +1,34 @@
+import enum
+
+from negotiations.domain.money import Money
+
+
+class Status(enum.StrEnum):
+    PENDING = "PENDING"
+    ACCEPTED = "ACCEPTED"
+    BROKEN_OFF = "BROKEN_OFF"
+
 
 class Negotiation:
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        seller_id: int,
+        buyer_id: int,
+        proposed_price: Money,
+        started_by_user_id: int,
+    ) -> None:
+        self._seller_id = seller_id
+        self._buyer_id = buyer_id
+        self._proposed_price = proposed_price
+        self._started_by_user_id = started_by_user_id
+        self._status = Status.PENDING
+
+    @property
+    def status(self) -> Status:
+        return self._status
+
+    def accept(self, user_id: int) -> None:
+        pass
+
+    def break_off(self, user_id: int) -> None:
         pass
--- used_stuff_market/negotiations/tests/test_negotiation.py
+++ used_stuff_market/negotiations/tests/test_negotiation.py
@@ -1,3 +1,30 @@
+from negotiations.domain.money import Money, USD
+from negotiations.domain.negotiation import Negotiation, Status
 
-def test_() -> None:
+SELLER_ID = 1
+BUYER_ID = 2
+OPENING_PRICE = Money(USD, "10.99")
+
+
+def test_seller_can_accept_negotiation_started_by_buyer() -> None:
+    negotiation = Negotiation(
+        SELLER_ID, BUYER_ID, OPENING_PRICE, started_by_user_id=BUYER_ID
+    )
+
+    negotiation.accept(SELLER_ID)
+
+    assert negotiation.status == Status.ACCEPTED
+
+
+def test_buyer_can_accept_negotiation_started_by_seller() -> None:
+    negotiation = Negotiation(
+        SELLER_ID, BUYER_ID, OPENING_PRICE, started_by_user_id=BUYER_ID
+    )
+
+    negotiation.accept(BUYER_ID)
+
+    assert negotiation.status == Status.ACCEPTED
+
+
+def test_accepted_negotiation_cannot_be_accepted_again() -> None:
     pass
