92e1430b6109dd62db67b2603e30ee3ba60cfc63
Sebastian Buczyński <nnplaya@gmail.com>
2025-11-04 21:06:21 +0100

    Negotiation aggregate tests

--- used_stuff_market/negotiations/tests/test_negotiation.py
+++ used_stuff_market/negotiations/tests/test_negotiation.py
@@ -1,5 +1,7 @@
+import pytest
+
 from negotiations.domain.money import Money, USD
-from negotiations.domain.negotiation import Negotiation, Status
+from negotiations.domain.negotiation import Negotiation, Status, NegotiationEnded
 
 SELLER_ID = 1
 BUYER_ID = 2
@@ -18,7 +20,7 @@ def test_seller_can_accept_negotiation_started_by_buyer() -> None:
 
 def test_buyer_can_accept_negotiation_started_by_seller() -> None:
     negotiation = Negotiation(
-        SELLER_ID, BUYER_ID, OPENING_PRICE, started_by_user_id=BUYER_ID
+        SELLER_ID, BUYER_ID, OPENING_PRICE, started_by_user_id=SELLER_ID
     )
 
     negotiation.accept(BUYER_ID)
@@ -27,4 +29,26 @@ def test_buyer_can_accept_negotiation_started_by_seller() -> None:
 
 
 def test_accepted_negotiation_cannot_be_accepted_again() -> None:
-    pass
+    negotiation = Negotiation(
+        SELLER_ID, BUYER_ID, OPENING_PRICE, started_by_user_id=SELLER_ID
+    )
+
+    negotiation.accept(BUYER_ID)
+
+    with pytest.raises(NegotiationEnded):
+        negotiation.accept(BUYER_ID)
+
+
+@pytest.mark.parametrize("started_by_user_id", [SELLER_ID, BUYER_ID])
+@pytest.mark.parametrize("side_breaking_off", [SELLER_ID, BUYER_ID])
+def test_broken_off_negotiation_cannot_be_broken_off_again(
+    started_by_user_id: int, side_breaking_off: int
+) -> None:
+    negotiation = Negotiation(
+        SELLER_ID, BUYER_ID, OPENING_PRICE, started_by_user_id=started_by_user_id
+    )
+
+    negotiation.break_off(side_breaking_off)
+
+    with pytest.raises(NegotiationEnded):
+        negotiation.break_off(side_breaking_off)
